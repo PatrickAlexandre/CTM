0fd056b5ecbfc77d607f53e851afcac0dee89ed3
PatrickAlexandre <PatrickAlexandre@users.noreply.github.com>
2025-01-11 00:21:14 +0100

    Initial commit from W3Schools

---
+++ ctm/EspacesVerts/Polyvalente/equipe.md
@@ -0,0 +1,5 @@
+Bruno VERRAES
+Anne-Laure VILLETTE
+Morgan SGUBBI
+Nathalie BLOT
+Patrick ASSIS
---
+++ ctm/employesData.js
@@ -0,0 +1,28 @@
+        const data = {
+            "regies": [
+                {
+                    "name": "Espaces Verts",
+                    "responsable": { "name": "Lionel RAVANI", "phone": "+33662033590" },
+                    "equipes": [
+                        { "name": "Nord", "members": [{ "name": "Emmanuel", "phone": "+33788836833" }, { "name": "Isabelle JACQUET", "phone": "+33123456789" }, { "name": "Jean-Pierre MORANDAIS", "phone": "+33123456789" }, { "name": "Cristophe TURCEY", "phone": "+33123456789" }, { "name": "José SANTOS", "phone": "+33123456789" }, { "name": "Florent BIANCOROSSO", "phone": "+33123456789" }] },
+                        { "name": "Sud", "members": [{ "name": "Said BERKANI", "phone": "+33123456789" }, { "name": "Habib COSKUN", "phone": "+33123456789" }, { "name": "Bruno LE COUTOUR", "phone": "+33123456789" }, { "name": "Mustafa AFSAR", "phone": "+33123456789" }] },
+                        { "name": "Centre", "members": [{ "name": "Julien CREMEL", "phone": "+33123456789" }, { "name": "Jean-Pierre MANCEL", "phone": "+33123456789" }, { "name": "Kévin BAIRRADA", "phone": "+33123456789" }, { "name": "Philippe LEVET", "phone": "+33123456789" }, { "name": "Iris de Riberolles", "phone": "+33652376154" }] },
+                        { "name": "Polyvalente", "members": [{ "name": "Bruno VERRAES", "phone": "+33625165363" }, { "name": "Anne-Laure VILLETTE", "phone": "+33123456789" }, { "name": "Morgan SGUBBI", "phone": "+33123456789" }, { "name": "Patrick ASSIS", "phone": "+33625921696" }] }
+                    ]
+                },
+                {
+                    "name": "Mécanique Propreté",
+                    "responsable": { "name": "Christophe PHILIPPE", "phone": "+33123456789" },
+                    "equipes": [
+                        { "name": "Atelier", "members": [{ "name": "Jean-Jacques CAPELLI", "phone": "+33669555761" }, { "name": "Thibault LEVAVASSEUR", "phone": "+33123456789" }, { "name": "Olivier DESWATINNES", "phone": "+33643364828" }, { "name": "Pascal RUGGIERO", "phone": "+33699245134" }, { "name": "Brahim MOUNGARI", "phone": "+33620113796" }] }
+                    ]
+                },
+                {
+                    "name": "Bureaux",
+                    "responsable": { "name": "Mélanie PILLAIS", "phone": "+33123456789" },
+                    "equipes": [
+                        { "name": "Informatique et Papiers", "members": [{ "name": "Nathalie PICAULT", "phone": "+33123456789" }, { "name": "Zoé VILLESALMON", "phone": "+33123456789" }, { "name": "Malika SAHRAOUI", "phone": "+33123456789" }, { "name": "Alexandra GODON", "phone": "+33643364828" }, { "name": "Matthieu BEAULIEU", "phone": "+33699245134" }] }
+                    ]
+                }
+            ]
+        };
